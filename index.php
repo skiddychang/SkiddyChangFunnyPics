d8c9ea40727cf9e82dccfa1d403bc819c299f157
skiddy <dprzybysz@kdrsolutions.pl>
2015-07-15 13:19:25 +0200

    asd

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 
 <html>
-<head>
-    <title>Funny Pics</title>
-    <meta charset="utf-8" />
+<title>Funny Pics</title>
+<link rel="stylesheet" href="/styles/default.css" />
+<meta charset="utf-8" />
 </head>
 <body>
-        asdasdasdasd
+    <div>
+        <div id="mainDiv">
+            <?php
+            $dir = 'images/';
+            $files = scandir("images/");
+            foreach($files as $file){
+                $filename = $dir.$file;
+                $ext = pathinfo($filename, PATHINFO_EXTENSION);
+                if($ext == 'webm'){
+                    echo "<div class='funnyPicDiv'><div class='funnyPic'><video src='$filename' controls></video></div></div>";
+                }
+                else if($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif'){
+                    echo "<div class='funnyPicDiv'><div class='funnyPic'><img src='$filename' /></div></div>";
+                }
+            }
 
+            ?>
+
+        </div>
+    </div>
 </body>
 </html>
